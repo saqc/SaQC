d68f485f27f3abbda951ca7f6eb82b2eb53dd11f
Bert Palm <bert.palm@ufz.de>
2023-12-14 16:38:37 +0100

    no warnings in test. guard for saqc 3.0

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,11 @@
 # SPDX-FileCopyrightText: 2021 Helmholtz-Zentrum für Umweltforschung GmbH - UFZ
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import saqc
+from packaging import version
+if version.parse(saqc.__version__) >= version.parse("3.0.0"):
+    saqc.options["legacy_call_style"] = False
+else:
+    saqc.options["legacy_call_style"] = True
+    saqc.options["legacy_call_style_warning"] = False
