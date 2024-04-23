804ed02cf51af49990f39a397c3ac17ffe2dc1c5
luenensc <peter.luenenschloss@ufz.de>
2024-04-23 23:48:53 +0200

    operators added

--- saqc/funcs/boolops.py
+++ saqc/funcs/boolops.py
@@ -2,21 +2,29 @@ from __future__ import annotations
 
 import operator
 import warnings
-from typing import TYPE_CHECKING, Any, Callable, Sequence
+from typing import TYPE_CHECKING, Any, Callable, Sequence, Union
 
 import numpy as np
 import pandas as pd
 
 import saqc
 
-OPERATOR_RELATION = {"&": "__le__", "|": "__ge__", "+": "__le__"}
+OPERATOR_RELATION = {"&": "__le__", "|": "__ge__", "+": "__le__", "-":"__le__", "*": "__le__", "/": "__le__", "**": "__le__"}
+OPERATOR_FUNCS = {'+': '__add__', '-': '__sub__',"*": "__mul__", "/": "__truediv__", "**": "__pow__"}
 
 if TYPE_CHECKING:
     from saqc import SaQC
 
 
 class BoolOpsMixin:
-    def _booloperate(self, other, operator, s_rep = None, o_rep=None):
+    def _booloperate(self, other, operator, s_rep = None, o_rep=None, o_kwargs=None):
+
+        if not isinstance(o_rep,list):
+            o_rep = [o_rep]*len(other.columns)
+        if not isinstance(o_kwargs, list):
+            o_kwargs = [o_kwargs] * len(other.columns)
+        o_rep = dict(zip(other.columns, o_rep))
+        o_kwargs = dict(zip(other.columns, o_kwargs))
         if len(self.columns) != len(other.columns):
             raise ValueError(
                 "boolean saqc object combination only supported for saqc objects of the same number of variables."
@@ -41,9 +49,9 @@ class BoolOpsMixin:
                 )
             if len(other._flags.history[of].meta) == 0:
                 other_meta =  {
-                        "func": o_rep or of,
+                        "func": o_rep[of] or of,
                         "args": (),
-                        "kwargs": {"dfilter": -np.inf, "field": of},
+                        "kwargs": o_kwargs[of] or {"dfilter": -np.inf, "field": of},
                     }
 
                 other_flags = other._flags[of]
@@ -95,51 +103,139 @@ class BoolOpsMixin:
             self._flags.history[f] = saqc.core.history.History(flagscol.index).append(flagscol, new_meta)
         return self
 
-    def __and__(self, other):
-        return self.copy(deep=True)._booloperate(other, "&")
+    def _arithmeticOperate(self, other, operator):
+        o_rep = None
+        if isinstance(other, np.ndarray):
+            other = pd.DataFrame(other)
+        if pd.api.types.is_scalar(other):
+            o_rep = str(other)
+            o_kwargs = {'value': o_rep, 'type':str(type(other)), 'squeezed':True}
+            other = saqc.SaQC([pd.Series(other, index=self.data[f].index, name=f) for f in self.data.columns],
+                              scheme=self.scheme)
+        elif isinstance(other, Union[pd.DataFrame, pd.Series]):
+            other = other.to_frame() if isinstance(other, pd.Series) else other
+            o_rep = other.columns.to_list()
+            o_kwargs = [{'value': f'column: {k}', 'type': str(other[k].dtype), 'squeezed': True} for k in other.columns]
+            other = saqc.SaQC(other, scheme=self.scheme)
 
-    def __or__(self, other):
-        return self.copy(deep=True)._booloperate(other, "|")
+        else:
+            other = other.__invert__()
+        out = self._booloperate(other, operator, o_rep=o_rep, o_kwargs=o_kwargs)
+        for f, of in zip(self.columns, other.columns):
+            out._data[f] = getattr(out.data[f],OPERATOR_FUNCS[operator])(other.data[of].values)
 
-    def __invert__(self):
-        out = saqc.SaQC(self.data, self.flags, scheme=self.scheme)
+        return out
+
+    def __and__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._booloperate(other.__invert__(), "&")
+
+    def __or__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._booloperate(other.__invert__(), "|")
+
+    def __invert__(self, s_rep=False):
+        out = self.copy(deep=True)
         for f in self.columns:
-            out._flags.history[f].append(self._flags[f], {"func": "squeezed", "args": (), "kwargs": {"field": f}})
+            flags = out._history[f].squeeze()
+            _meta = out._history[f].meta
+            if len(_meta) > 0:
+                ops = [f for f in _meta[-1]['kwargs'] if f.startswith('operand')]
+                if len(ops) > 0:
+                    unsqueezed = [o for o in ops if not _meta[-1]['kwargs'][o].get('squeezed', False)]
+                    if s_rep:
+                        func_entry = f"|{f}|"
+                        kwargs_entry = {'operand_0':{'squeezed':True, 'type': 'field', 'value': f'{f}_data'}}
+                    else:
+                        func_entry = f"{_meta[-1]['func']}"
+                        kwargs_entry = _meta[-1]['kwargs']
+                        if len(unsqueezed) > 0:
+                            func_entry = f"|{func_entry}|"
+                            for o in unsqueezed:
+                                kwargs_entry[o].update({'squeezed':True})
+                else:
+                    func_entry = f"|{f}|"
+                    kwargs_entry = {'operand_0':{'squeezed':True}}
+
+                meta = {"func": func_entry, "args": (), "kwargs": kwargs_entry}
+                history = saqc.core.History(flags.index).append(flags, meta=meta)
+                out._history[f] = history
         return out
 
-    def __lt__(self, other):
-        out = self.copy(deep=True)
-        for f, of in zip(out.columns,other.columns):
-            out._flags.history[f].append(other._flags.history[of].copy(deep=True))
-        return out
+    def __ilshift__(self, other):
+        return self.__lshift__(other)
 
-    def __gt__(self, other):
-        return other.__lt__(self)
-
-    def __le__(self, other):
-        out = self.copy(deep=True)
-        out._data = other.data
-        return out
-    def __ge__(self, other):
-        return other.__le__(self)
+    def __lshift__(self, other, d_vals='left'):
+        self=self.copy(deep=True)
+        for f, of in zip(self.columns, other.columns):
+            if len(other._history[of].meta) > 0:
+                for k in range(len(other._history[of].meta)):
+                    self._flags.history[f].append(other._history[of].hist.iloc[:,k], other._flags.history[of].meta[k])
+            else:
+                self._flags.history[f].append(other._flags[of], {'func':'append', 'args':(), 'kwargs':{'field':of}})
+        if d_vals=='right':
+            self._data = other._data
+        return self
 
 
     def __iand__(self, other):
-        return self.__invert__().__and__(other.__invert__())
+        new_cols = self.__and__(other, self_assign=True)
+        for f in self.columns:
+            self._history[f].append(new_cols._flags[f], meta=new_cols._history[f].meta[-1])
+
+        return self
 
     def __ior__(self, other):
-        return self.__invert__().__or__(other.__invert__())
+        new_cols = self.__or__(other, self_assign=True)
+        for f in self.columns:
+            self._history[f].append(new_cols._flags[f], meta=new_cols._history[f].meta[-1])
 
-    def __add__(self, other):
-        o_rep=None
-        if pd.api.types.is_scalar(other):
-            o_rep=str(other)
-            other = saqc.SaQC([pd.Series(other, index=self.data[f].index, name=f) for f in self.data.columns],
-                              scheme=self.scheme)
+        return self
 
-        out = self._booloperate(other, '+', o_rep=o_rep)
-        for f, of in zip(self.columns, other.columns):
-            out._data[f] = out.data[f] + other.data[of]
+    def __add__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._arithmeticOperate(other, '+')
 
-        return out
+    def __radd__(self, other):
+        return self.__add__(other, d_vals='right')
 
+    def __iadd__(self, other):
+        return self.__lshift__(self.__add__(other, self_assign=True), d_vals='right')
+
+    def __sub__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._arithmeticOperate(other, '-')
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __isub__(self, other):
+        return self.__lshift__(self.__sub__(other, self_assign=True), d_vals='right')
+
+    def __truediv__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._arithmeticOperate(other, '/')
+
+    def __rtruediv__(self, other):
+        return self.__pow__(-1).__mul__(other)
+
+    def __itruediv__(self, other):
+        return self.__lshift__(self.__truediv__(other, self_assign=True), d_vals='right')
+    def __mul__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._arithmeticOperate(other, '*')
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __imul__(self, other):
+        return self.__lshift__(self.__mul__(other, self_assign=True), d_vals='right')
+
+    def __pow__(self, other, self_assign=False):
+        return self.__invert__(s_rep=self_assign)._arithmeticOperate(other, '**')
+
+    def __rpow__(self, other):
+        raise NotImplementedError('not implemented')
+
+    def __ipow__(self, other):
+        return self.__lshift__(self.__pow__(other, self_assign=True), d_vals='right')
+
+    def __xor__(self, other):
+        return self.__lshift__(other, d_vals='right')
+
+    def __ixor__(self, other):
+        return self.__xor__(other)
